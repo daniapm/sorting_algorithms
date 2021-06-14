c98c71ca6948e62f6fc4456261f75a004aaafbf9
Juansu01 <thejuank0@hotmail.com>
2021-06-14 14:33:54 -0500

    Added else block

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,16 +14,6 @@ void selection_sort(int *array, size_t size)
 	/*avanzar la posición a través de toda la matriz */
 	size_t i, j, position, swap;
 
-	if (size < 2)
-	{
-		return;
-	}
-
-	if (array == NULL)
-	{
-		return;
-	}
-
 	if (array != NULL && size > 2)
 	{
 		/*encuentra el elemento min en el sin clasificar a [i .. aLength-1] */
@@ -47,5 +37,9 @@ void selection_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
+	}
+	else
+	{
+		return;
 	}
 }
