3d537cdcffbc989bf6050bc0688a71f2fafaf445
Juansu01 <thejuank0@hotmail.com>
2021-06-14 14:45:20 -0500

    Reverted changes

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -16,13 +16,11 @@ void quick_sort(int *array, size_t size)
 	lw = 0;
 	hi = size - 1;
 
-	if (array != NULL || size < 2)
+	if (array != NULL && size > 2)
 	{
-		return;
-	}
 
 	_quick_sort(array, lw, hi, size);
-
+	}
 }
 
 /**
