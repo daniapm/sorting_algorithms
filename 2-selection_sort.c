8b3f8c369e7f527876ee38ce0a38bbb6afa7f10e
Juansu01 <thejuank0@hotmail.com>
2021-06-14 11:16:57 -0500

    Fixed code style and added Betty parameters

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,33 +2,40 @@
 #include <stdio.h>
 #include "sort.h"
 
+/**
+* selection_sort - This function implements the selection sort algorithm.
+* @array: Pointer to the array that will be sorted.
+* @size: Size of the array.
+* Return: Doesn't return.
+*/
+
 void selection_sort(int *array, size_t size)
 {
-    /*avanzar la posición a través de toda la matriz */
-    size_t i, j, position, swap;
+	/*avanzar la posición a través de toda la matriz */
+	size_t i, j, position, swap;
 
-    if (array != NULL && size > 2)
-    {
-    /*encuentra el elemento min en el sin clasificar a [i .. aLength-1] */
-    for(i = 0; i < size - 1; i++)
-    {
-    /*asume que el min es el primer elemento */
-    position=i;
-    /* prueba contra elementos después de i para encontrar el más pequeño */
-    for(j = i + 1; j < size; j++)
-    {
-    /* si este elemento es menor, entonces es el nuevo mínimo */
-    if(array[position] > array[j])
-    /* encontró un nuevo mínimo; recuerda su índice */
-    position = j;
-    }
-    if(position != i)
-    {
-    swap = array[i];
-    array[i] = array[position];
-    array[position] = swap;
-    print_array(array, size);
-    }
-    }
-    }
+	if (array != NULL && size > 2)
+	{
+		/*encuentra el elemento min en el sin clasificar a [i .. aLength-1] */
+		for (i = 0; i < size - 1; i++)
+		{
+			/*asume que el min es el primer elemento */
+			position = i;
+			/* prueba contra elementos después de i para encontrar el más pequeño */
+			for (j = i + 1; j < size; j++)
+			{
+				/* si este elemento es menor, entonces es el nuevo mínimo */
+				if (array[position] > array[j])
+				/* encontró un nuevo mínimo; recuerda su índice */
+				position = j;
+			}
+			if (position != i)
+			{
+				swap = array[i];
+				array[i] = array[position];
+				array[position] = swap;
+				print_array(array, size);
+			}
+		}
+	}
 }
