abb20cdaa037e0c1c51da249169e21848c11dd58
Juansu01 <thejuank0@hotmail.com>
2021-06-16 08:15:42 -0500

    Pls

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -31,38 +31,36 @@ void insertion_sort_list(listint_t **list)
 			tmp = ptr->prev;
 			while (tmp != NULL && tmp->n > ptr->n) /*empezamos a comparar*/
 			{
-				printf("while: temp = %d, ptr = %d\n", tmp->n, ptr->n);
+				/*printf("while: temp = %d, ptr = %d\n", tmp->n, ptr->n);*/
 			
 				tmp->next = ptr->next;
-				printf("temp->next = %d\n", tmp->next->n);
 				if (ptr->next !=  NULL)
 				{
 					ptr->next->prev = ptr->prev;
-					printf("ptr->next->prev->n = %d\n", ptr->next->prev->n);
 				}
 				if (tmp->prev == NULL) /*este es el nodo mas pequeño*/
 				{
-					tmp = *list;
+					/*tmp = *list;*/
 					ptr->prev = NULL;
 					ptr->next = tmp;
 					ptr->next->prev = ptr;
 					*list = ptr;
 					print_list(*list);
+					break;
 				}
 				else
 				{	
 					
-					tmp = tmp->next;
+					/*tmp = tmp->next;*/
 					tmp->prev->next = ptr;
-					printf("tmp->prev->next->n = %d\n", tmp->prev->next->n);
 					ptr->prev = tmp->prev;
-					printf("ptr->prev->n = %d\n", ptr->prev->n);
 					tmp->prev = ptr;
 					ptr->next = tmp;
 					print_list(*list);
+					/*printf("else: temp = %d, ptr = %d\n", tmp->n, ptr->n);*/
 				}
-				tmp = tmp->prev;	
-
+				tmp = tmp->prev->prev;
+				/*printf("final: temp = %d, ptr = %d\n", tmp->n, ptr->n);*/
 			}
 		}
 	}
