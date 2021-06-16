76fbec5a9c28de7bd394502eed3fe5515b8d17e0
Dania Puertas <daniajibellis@hotmail.com>
2021-06-16 02:15:04 +0000

    file header

--- sort.h
+++ sort.h
@@ -28,5 +28,5 @@ int partition(int *array, int lw, int hi, size_t size);
 void _swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void shell_sort(int *array, size_t size);
-
+void counting_sort(int *array, size_t size);
 #endif /*SORT_H */
